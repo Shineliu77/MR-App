359b583ce645184912334f1c5dbac1f6faa6970a
Shineliu77 <33471613+Shineliu77@users.noreply.github.com>
2026-03-27 11:59:33 +0800

    Game-1

--- Assets/TutorialInfo/Scripts/Readme.cs
+++ Assets/TutorialInfo/Scripts/Readme.cs
@@ -1,32 +1,16 @@
-﻿/*
-================================
-Assets for Unity by Makaka Games
-================================
- 
-[Online  Docs -> Updated]: https://makaka.org/unity-assets
-[Offline Docs - PDF file]: find it in the package folder.
-
-[Support]: https://makaka.org/support
-
-Copyright © 2025 Andrey Sirota (Makaka Games)
-*/
-
+﻿using System;
 using UnityEngine;
 
-using System;
-
-namespace MakakaGames.Readme
+public class Readme : ScriptableObject
 {
-    public class Readme : ScriptableObject
-    {
-        public Texture2D icon;
-        public string title;
-        public Section[] sections;
+    public Texture2D icon;
+    public string title;
+    public Section[] sections;
+    public bool loadedLayout;
 
-        [Serializable]
-        public class Section
-        {
-            public string heading, text, linkText, url;
-        }
+    [Serializable]
+    public class Section
+    {
+        public string heading, text, linkText, url;
     }
 }
